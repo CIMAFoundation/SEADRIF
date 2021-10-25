ff8112b24ac24e7a3d33c071fce7453dc1634901
alessandro@acrotec.it <alessandro@acrotec.it>
2021-10-25 15:57:31 +0200

    download log e user settings in whoami

--- server/rfsea/__init__.py
+++ server/rfsea/__init__.py
@@ -1,4 +1,6 @@
 
+import csv
+import io
 from rfsea.models import LogActivity
 import traceback
 
@@ -12,4 +14,19 @@ def add_logbook_activity(request, activity):
     except:
         traceback.print_exc()
     
+
+def get_logbook_csv(dt_from, dt_to):
+    
+    activities = LogActivity.objects.filter(dt__range=(dt_from, dt_to))
+    
+    output = io.BytesIO()
+    writer = csv.writer(output, dialect=csv.excel, delimiter=';')
+    writer.writerow([u'user', 'date', u'activity'])
+    for activity in activities:
+        writer.writerow([activity.user, activity.dt, activity.activity]) 
+
+    csv_string = output.getvalue()
+    output.close()
+    
+    return csv_string
     
